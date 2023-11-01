7066798b9d4d7a4f91769bc7d6f8c225113a27f6
Aliaksandr <djvalentinoff@gmail.com>
2023-11-01 11:50:54 +0300

    fix: show header

--- src/js/components/header-show.js
+++ src/js/components/header-show.js
@@ -1,8 +1,34 @@
 document.addEventListener("DOMContentLoaded", () => {
+  const header = document.querySelector(".header");
+  let lastScrollTop = 0;
+  let delta = 5;
+  let isHeaderVisible = false;
+
   window.addEventListener("scroll", () => {
-    const header = document.querySelector(".header");
-    window.scrollY > 120
-      ? header.classList.add("active")
-      : header.classList.remove("active");
+    const st = window.scrollY;
+
+    if (Math.abs(lastScrollTop - st) <= delta) return;
+
+    if (st <= 200) {
+      // Если прокрутка находится в пределах первых 120px, скрываем хедер
+      if (isHeaderVisible) {
+        header.classList.remove("active");
+        isHeaderVisible = false;
+      }
+    } else if (st > lastScrollTop) {
+      // Если скролл вниз, скрываем хедер
+      if (isHeaderVisible) {
+        header.classList.remove("active");
+        isHeaderVisible = false;
+      }
+    } else {
+      // Если скролл вверх, показываем хедер
+      if (!isHeaderVisible) {
+        header.classList.add("active");
+        isHeaderVisible = true;
+      }
+    }
+
+    lastScrollTop = st;
   });
 });
