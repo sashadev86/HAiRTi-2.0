d7fedb59308d98bace9083ab8a13424d90d398d8
Aliaksandr <djvalentinoff@gmail.com>
2023-11-14 11:33:36 +0300

    build main

--- src/js/_components.js
+++
@@ -1,9 +0,0 @@
-import './components/animations';
-import './components/header-show';
-import './components/promo-input';
-import './components/swiper-hero';
-import './components/validation';
-import './components/swiper-examples';
-import './components/fancybox';
-import './components/map';
-import './components/modal-success';
